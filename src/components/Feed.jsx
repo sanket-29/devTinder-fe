b6154c4ccbfb331c9f84c702bbb3e035348e9da1
sanket-29 <avkirkarsanket29@gmail.com>
2026-03-26 21:36:00 +0530

    build feed ui

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { BASE_URL } from '../utils/constants';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import { addFeed } from '../utils/feedSlice';
+import { useEffect } from 'react';
+import UserCard from './UserCard';
 
 const Feed = () => {
-  return (
-    <div>
-      <h1>Feed</h1>
-    </div>
-  )
-}
+  
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+  
+  const getFeed = async () => {
+    if(feed) return; // if feed data already exists in the store, no need to fetch again
 
-export default Feed
+    try {
+      const res = await axios.get(`${BASE_URL}/user/feed`, { withCredentials: true });
+      
+      dispatch(addFeed(res.data));
+    } catch (error) {
+      console.error("Error fetching feed:", error);
+    }
+  }
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  return (
+    feed && (
+    <div className ='flex justify-center my-10'>
+      <UserCard user={feed.data[0]} />
+    </div>
+    )
+  );
+};
+
+export default Feed;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 
 const Profile = () => {
   return (
@@ -6,6 +6,6 @@ const Profile = () => {
       profile page
     </div>
   )
-}
+};
 
-export default Profile
+export default Profile;
